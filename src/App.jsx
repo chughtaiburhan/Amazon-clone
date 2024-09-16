2fdf385740036db7ed85877106cf6f8d7087ce96
chughtaiburhan <burhanchughtai90@gmail.com>
2024-09-16 21:25:33 +0500

    Header completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+ 
+  return (
+
+    <div className="app">
+      <Header/>
+      
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClloMTzOnitT_XbJcHLqPQoAVbdWKEOsk",
+    authDomain: "clone-91e80.firebaseapp.com",
+    projectId: "clone-91e80",
+    storageBucket: "clone-91e80.appspot.com",
+    messagingSenderId: "108102303907",
+    appId: "1:108102303907:web:269bdf630ba2043f2a7f16"
+  };
